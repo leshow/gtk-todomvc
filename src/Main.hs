a862ab0e5444f91be615cbb87f373df9ad5aaca9
Evan Cameron <evan@vectorface.com>
2019-01-17 13:52:33 -0500

    can add items and write in text field

--- src/Main.hs
+++ src/Main.hs
@@ -5,22 +5,65 @@ module Main where
 import qualified GI.Gtk                             as Gtk
 import           GI.Gtk.Declarative
 import           GI.Gtk.Declarative.App.Simple
+import qualified Data.Text                          as T
+import           Data.Text                           ( Text )
+import qualified Data.Vector                        as V
+import           Data.Vector                         ( Vector )
+import           Control.Monad                       ( void )
 
-type State = ()
-data Event = Closed
+data State = State
+  { todos :: Vector Item
+  , textbox :: Text
+  }
 
+data Event
+  = TextChanged Text
+  | TodoAdded
+  | Completed Int
+  | Closed
+
+data Item = Item
+  { name :: Text
+  }
 
 main :: IO ()
-main =
-  run App { view = view', update = update', inputs = [], initialState = () }
+main = void $ run App
+  { view         = view'
+  , update       = update'
+  , inputs       = []
+  , initialState = State { todos = mempty, textbox = mempty }
+  }
 
 view' :: State -> AppView Gtk.Window Event
-view' s = bin 
-  Gtk.Window 
-  [#title := "Demo"
-  , on #deleteEvent (const (True, Closed))
-  ] 
-  $ widget Gtk.Label [#label := "Hello, World!"]
+view' s = bin
+  Gtk.Window
+  [#title := "TodoGTK+", on #deleteEvent (const (True, Closed))]
+  (container Gtk.Box
+             [#orientation := Gtk.OrientationVertical]
+             [todoList, addTodo]
+  )
+
+ where
+  todoList = container Gtk.Box
+                       [#orientation := Gtk.OrientationVertical]
+                       (fmap todoitem (todos s))
+  todoitem item = widget Gtk.Label [#label := name item]
+  addTodo = widget
+    Gtk.Entry
+    [ #text := textbox s
+    , #placeholderText := "What do you need to do?"
+    , onM #changed (fmap TextChanged . Gtk.entryGetText)
+    , on #activate TodoAdded
+    ]
 
 update' :: State -> Event -> Transition State Event
-update' _ Closed = Exit
+update' s e = case e of
+  Closed    -> Exit
+  TodoAdded -> Transition
+    s { todos   = V.snoc (todos s) (Item { name = textbox s })
+      , textbox = T.empty
+      }
+    (pure Nothing)
+  TextChanged t -> Transition s { textbox = t } (pure Nothing)
+  _             -> Exit
+
